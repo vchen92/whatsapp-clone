8b6a02b49853900ae5a949f524d2a6cf8b8c3e3c
Vincent Chen <vince_1728@hotmail.com>
2020-12-15 14:06:16 -0800

    Login page skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
 import Chat from './Chat/Chat';
 import Sidebar from './Sidebar/Sidebar';
+import Login from './Login/Login';
+
+import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
 		<div className="app">
-			<div className="app__body">
-				<Router>
-					<Sidebar />
-					<Switch>
-						<Route path="/rooms/:roomId">
-							<Chat />
-						</Route>
-						<Route path="/">{/* <Chat /> */}</Route>
-					</Switch>
-				</Router>
-			</div>
-		</div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">{/* <Chat /> */}</Route>
+            </Switch>
+          </Router>
+        </div>      
+      )}
+    </div>			
   );
 }
 

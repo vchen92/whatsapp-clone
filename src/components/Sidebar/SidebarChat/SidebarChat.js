9572dbb3522d87e552e6cc850d787f1abf9fb39c
Vincent Chen <vince_1728@hotmail.com>
2020-12-16 03:47:49 -0800

    show last message

--- src/components/Sidebar/SidebarChat/SidebarChat.js
+++ src/components/Sidebar/SidebarChat/SidebarChat.js
@@ -6,10 +6,23 @@ import db from './../../../firebase';
 
 function SidebarChat({ addNewChat, name, id }) {
   const [seed, setSeed] = useState('');
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  useEffect(() => {
+    if (id) {
+      db
+      .collection('rooms').doc(id)
+      .collection('messages').orderBy('timestamp', 'desc')
+      .onSnapshot(snap => (
+        setMessages(snap.docs.map(doc => 
+          doc.data())))
+      );
+    }
+  })
 
   const createChat = () => {
     const roomName = prompt('Please enter name for chat');
@@ -21,13 +34,17 @@ function SidebarChat({ addNewChat, name, id }) {
     }
   }
 
+  const lastMessage = () => (
+    messages[0]?.message
+  )
+
   return !addNewChat ? (
     <Link to={`/rooms/${id}`}>
       <div className="sidebarChat">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="sidebarChat__info">
           <h2>{name}</h2>
-          <p>Last message...</p>
+          <p>{lastMessage()}</p>
         </div>
       </div>
     </Link>
